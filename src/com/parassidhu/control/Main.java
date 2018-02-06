82d62035a545eb0e5bb6e9d5b5f94cee74bf740f
Paras Sidhu <sidhuparas1234@gmail.com>
2018-02-06 21:04:09 +0530

    Added TimeForAChange
    Please check Main.java to check how program works

---
+++ src/com/parassidhu/control/Main.java
@@ -0,0 +1,20 @@
+package com.parassidhu.control;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which program would you like to run (Enter Class Name): ");
+        switch (scanner.nextLine()){
+            case "TimeForAChange":
+                TimeForAChange.execute();
+                break;
+            default:
+                System.out.println("Please enter a valid input.");
+                break;
+        }
+
+    }
+}
---
+++ src/com/parassidhu/control/TimeForAChange.java
@@ -0,0 +1,23 @@
+package com.parassidhu.control;
+
+public class TimeForAChange {
+
+    public static void execute(){
+        // What would be the output of this program?
+        // What do you think? Any guesses
+        // It mayn't be what you think. Try executing to see
+        // Answer at end of the file
+
+        System.out.println(2.00-1.10);
+    }
+}
+
+
+
+
+
+
+
+// The different output is due to the fact that not all decimals can
+// be represented exactly using binary floating-point
+// For accuracy, we need to use BigDecimal
